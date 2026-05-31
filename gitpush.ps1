9a06e695ac715b9d4386763fa0e36bbae6884079
dev-aemni <ghoshbapi205@gmail.com>
2026-05-31 22:49:30 +0530

    V8361B8-24C69-812A-9B8A-1F2E5C3D4E5F

--- gitpush.ps1
+++ gitpush.ps1
@@ -1,11 +1,3 @@
-git init
-
 git add .
-
 git commit -m "V8361B8-24C69-812A-9B8A-1F2E5C3D4E5F"
-
-git branch -M main
-
-git remote add origin https://github.com/dev-aemni/bakbak.git
-
 git push -u origin main
